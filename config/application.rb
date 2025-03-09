749af218821a97c2ef040d6c1ce595f149e59f1d
daisuke289 <daisuke289@gmail.com>
2025-03-09 20:34:52 +0900

    修正２

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,9 @@ require "action_view/railtie"
 require "action_cable/engine"
 # require "rails/test_unit/railtie"
 
+# dotenvを早期に読み込む
+require 'dotenv/load'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
